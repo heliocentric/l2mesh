bab7d1dc8ccb83b05d4b2da0b95a5698672a9aae
Dylan Cochran <heliocentric@gmail.com>
2016-04-15 10:50:03 -0400

    Fix l2mesh on puppet4

---
+++ manifests/host.pp
@@ -0,0 +1,34 @@
+define l2mesh::host(
+	$fqdn = $name,
+	$host,
+	$ip,
+	$port,
+	$tcp_only,
+	$public_key,
+	$tag_conf,
+	$conf = undef,
+) {
+	
+	file { $host:
+		owner   => root,
+	    group   => root,
+		mode    => '0444',
+	    require => File[$hosts],
+		notify	=> Exec[$reload],
+	    before  => Service[$service],
+		tag     => $tag,
+	    content     => "Address = ${ip}
+Port = ${port}
+Compression = 0
+TCPOnly = ${tcp_only}
+
+${public_key}
+",
+
+	}
+	concat::fragment { "${tag_conf}_${fqdn}":
+		target  => $conf,
+		tag     => "${tag_conf}_${fqdn}",
+		content => "ConnectTO = ${fqdn}\n",
+	}
+}

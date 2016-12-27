3476a4704f6f77bdfc7ae044dabe66980476fc18
Dylan Cochran <heliocentric@gmail.com>
2016-12-26 20:28:37 -0500

    Fix tabs in host

--- manifests/host.pp
+++ manifests/host.pp
@@ -1,34 +1,35 @@
+# vim: set expandtab:
 define l2mesh::host(
-	$fqdn = $name,
-	$host,
-	$ip,
-	$port,
-	$tcp_only,
-	$public_key,
-	$tag_conf,
-	$conf = undef,
+  $fqdn = $name,
+  $host,
+  $ip,
+  $port,
+  $tcp_only,
+  $public_key,
+  $tag_conf,
+  $conf = undef,
 ) {
-	
-	file { $host:
-		owner   => root,
-	    group   => root,
-		mode    => '0444',
-	    require => File[$hosts],
-		notify	=> Exec[$reload],
-	    before  => Service[$service],
-		tag     => $tag,
-	    content     => "Address = ${ip}
-Port = ${port}
-Compression = 0
-TCPOnly = ${tcp_only}
 
-${public_key}
-",
+  file { $host:
+    owner   => root,
+    group   => root,
+    mode    => '0444',
+    require => File[$hosts],
+    notify  => Exec[$reload],
+    before  => Service[$service],
+    tag     => $tag,
+    content => "Address = ${ip}
+    Port = ${port}
+    Compression = 0
+    TCPOnly = ${tcp_only}
 
-	}
-	concat::fragment { "${tag_conf}_${fqdn}":
-		target  => $conf,
-		tag     => "${tag_conf}_${fqdn}",
-		content => "ConnectTO = ${fqdn}\n",
-	}
+    ${public_key}
+    ",
+
+  }
+  concat::fragment { "${tag_conf}_${fqdn}":
+    target  => $conf,
+    tag     => "${tag_conf}_${fqdn}",
+    content => "ConnectTO = ${fqdn}\n",
+  }
 }
